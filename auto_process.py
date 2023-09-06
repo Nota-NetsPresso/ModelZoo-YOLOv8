c051c020a67a6f9ea0ae1a97531252569973eaa7
Junho Shin <junho.shin@nota.ai>
2023-09-06 13:14:45 +0900

    Model compression with PyNP

--- auto_process.py
+++ auto_process.py
@@ -1,6 +1,7 @@
 import argparse
 
 from loguru import logger
+from netspresso.compressor import ModelCompressor, Task, Framework
 
 from ultralytics import YOLO
 
@@ -11,8 +12,17 @@ def parse_args():
     """
         Common arguments
     """
+    parser.add_argument('-n', '--name', type=str, default='yolov8n', help='model name')
     parser.add_argument('-w', '--weights', type=str, default='./yolov8n.pt', help='weights path')
 
+    """
+        Compression arguments
+    """
+    parser.add_argument("--compression_method", type=str, choices=["PR_L2", "PR_GM", "PR_NN", "PR_ID", "FD_TK", "FD_CP", "FD_SVD"], default="PR_L2")
+    parser.add_argument("--recommendation_method", type=str, choices=["slamp", "vbmf"], default="slamp")
+    parser.add_argument("--compression_ratio", type=int, default=0.5)
+    parser.add_argument("-m", "--np_email", help="NetsPresso login e-mail", type=str)
+    parser.add_argument("-p", "--np_password", help="NetsPresso login password", type=str)
 
     return parser.parse_args()
 
@@ -21,6 +31,7 @@ if __name__ == '__main__':
     args = parse_args()
 
     model = YOLO(args.weights)
+    model_args = model.model.args
 
     """ 
         Convert YOLOv8 model to fx 
@@ -32,3 +43,38 @@ if __name__ == '__main__':
 
     logger.info("yolov8 to fx graph end.")
     
+    """
+        Model compression - recommendation compression 
+    """
+    logger.info("Compression step start.")
+    
+    compressor = ModelCompressor(email=args.np_email, password=args.np_password)
+
+    UPLOAD_MODEL_NAME = args.name
+    TASK = Task.OBJECT_DETECTION
+    FRAMEWORK = Framework.PYTORCH
+    UPLOAD_MODEL_PATH = 'model_fx.pt'
+    INPUT_SHAPES = [{"batch": 1, "channel": 3, "dimension": (model_args['imgsz'], model_args['imgsz'])}]
+    model = compressor.upload_model(
+        model_name=UPLOAD_MODEL_NAME,
+        task=TASK,
+        framework=FRAMEWORK,
+        file_path=UPLOAD_MODEL_PATH,
+        input_shapes=INPUT_SHAPES,
+    )
+
+    COMPRESSION_METHOD = args.compression_method
+    RECOMMENDATION_METHOD = args.recommendation_method
+    RECOMMENDATION_RATIO = args.compression_ratio
+    COMPRESSED_MODEL_NAME = f'{UPLOAD_MODEL_NAME}_{COMPRESSION_METHOD}_{RECOMMENDATION_RATIO}'
+    OUTPUT_PATH = COMPRESSED_MODEL_NAME + '.pt'
+    compressed_model = compressor.recommendation_compression(
+        model_id=model.model_id,
+        model_name=COMPRESSED_MODEL_NAME,
+        compression_method=COMPRESSION_METHOD,
+        recommendation_method=RECOMMENDATION_METHOD,
+        recommendation_ratio=RECOMMENDATION_RATIO,
+        output_path=OUTPUT_PATH,
+    )
+
+    logger.info("Compression step end.")
